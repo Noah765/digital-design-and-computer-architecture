9b553fdf7a7c622aa452246b4fa4c1ccb411e39a
Noah765 <noah.landgraf@gmx.de>
2026-03-20 11:58:52 +0100

    lab3/session

---
+++ lab3/session/Top.v
@@ -0,0 +1,19 @@
+module Top (
+  input  [3:0] a,
+  input  [3:0] b,
+  output [6:0] segments,
+  output       overflow
+);
+  wire [4:0] s;
+
+  Adder g1 (
+    .a(a),
+    .b(b),
+    .s(s)
+  );
+  Decoder g2 (
+    .data    (s[3:0]),
+    .segments(segments)
+  );
+  assign overflow = s[4];
+endmodule
